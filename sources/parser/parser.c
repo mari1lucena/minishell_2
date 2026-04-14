cb4dd6eb54324b44954947a621b9efeacc02b869
made-jes <chivas.maria17@hotmail.com>
2026-04-14 20:51:48 +0100

    quoted operators and message error

--- sources/parser/parser.c
+++ sources/parser/parser.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parser.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marilins <marilins@student.42.fr>          +#+  +:+       +#+        */
+/*   By: made-jes <made-jes@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/15 20:00:48 by made-jes          #+#    #+#             */
-/*   Updated: 2026/03/31 14:47:19 by marilins         ###   ########.fr       */
+/*   Updated: 2026/04/14 20:50:34 by made-jes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,38 @@ int	syntax_error(char *token)
 	return (1);
 }
 
+static int	check_operators(t_token *tokens)
+{
+	if ((tokens->type == PIPE || tokens->type == REDIR_IN
+			|| tokens->type == REDIR_OUT || tokens->type == APPEND
+			|| tokens->type == HEREDOC) && tokens->was_quoted)
+	{
+		fprintf(stderr, "%s: command not found\n", tokens->value);
+		get_shell()->last_exit = 127;
+		return (1);
+	}
+	return (0);
+}
+
+static int	validate_token_syntax(t_token *tokens, t_token *prev)
+{
+	if (tokens->type == PIPE)
+	{
+		if (!prev || prev->type == PIPE)
+			return (syntax_error("|"));
+		if (!tokens->next || tokens->next->type == PIPE)
+			return (syntax_error("|"));
+	}
+	if (tokens->type == REDIR_IN || tokens->type == REDIR_OUT
+		|| tokens->type == APPEND || tokens->type == HEREDOC)
+	{
+		if (!tokens->next || (tokens->next->type != WORD
+				&& tokens->next->type != STR))
+			return (syntax_error(tokens->value));
+	}
+	return (0);
+}
+
 int	validate_syntax(t_token *tokens)
 {
 	t_token	*prev;
@@ -25,89 +57,12 @@ int	validate_syntax(t_token *tokens)
 	prev = NULL;
 	while (tokens)
 	{
-		if (tokens->type == PIPE)
-		{
-			if (!prev || prev->type == PIPE)
-				return (syntax_error("|"));
-			if (!tokens->next || tokens->next->type == PIPE)
-				return (syntax_error("|"));
-		}
-		if (tokens->type == REDIR_IN || tokens->type == REDIR_OUT
-			|| tokens->type == APPEND || tokens->type == HEREDOC)
-		{
-			if (!tokens->next || (tokens->next->type != WORD
-					&& tokens->next->type != STR))
-				return (syntax_error(tokens->value));
-		}
+		if (check_operators(tokens))
+			return (1);
+		if (validate_token_syntax(tokens, prev))
+			return (1);
 		prev = tokens;
 		tokens = tokens->next;
 	}
 	return (0);
-}
-
-int	count_args(t_token *start, t_token *end)
-{
-	int		count;
-	t_token	*tokens;
-
-	count = 0;
-	tokens = start;
-	while (tokens && tokens != end)
-	{
-		if (tokens->type == WORD || tokens->type == STR)
-			count++;
-		else if (tokens->type == REDIR_IN || tokens->type == REDIR_OUT
-			|| tokens->type == APPEND || tokens->type == HEREDOC)
-			tokens = tokens->next;
-		tokens = tokens->next;
-	}
-	return (count);
-}
-
-void	fill_args(t_ast *node, t_token *start, t_token *end)
-{
-	int		i;
-	t_token	*tokens;
-
-	i = 0;
-	tokens = start;
-	while (tokens && tokens != end)
-	{
-		if (tokens->type == WORD || tokens->type == STR)
-		{
-			if (tokens->value[0] != '\0' || tokens->was_quoted)
-				node->cmd_args[i++] = ft_strdup(tokens->value);
-		}
-		else if (tokens->type == REDIR_IN || tokens->type == REDIR_OUT
-			|| tokens->type == APPEND || tokens->type == HEREDOC)
-			tokens = tokens->next;
-		tokens = tokens->next;
-	}
-	node->cmd_args[i] = NULL;
-}
-
-void	add_redir(t_ast *node, int type, char *filename, int expand)
-{
-	t_redir	*redir;
-	t_redir	*current;
-
-	redir = malloc(sizeof(t_redir));
-	if (!redir)
-		return ;
-	redir->type = type;
-	redir->filename = ft_strdup(filename);
-	redir->expand = expand;
-	redir->delimiter = NULL;
-	redir->quoted = 0;
-	redir->heredoc_file = NULL;
-	redir->next = NULL;
-	if (!node->redirs)
-		node->redirs = redir;
-	else
-	{
-		current = node->redirs;
-		while (current->next)
-			current = current->next;
-		current->next = redir;
-	}
 }
--- sources/parser/utils_parser_aux.c
+++ sources/parser/utils_parser_aux.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   utils_parser_aux.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: made-jes <made-jes@student.42.fr>          +#+  +:+       +#+        */
+/*   By: made-jes <made-jes@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/15 20:00:48 by made-jes          #+#    #+#             */
-/*   Updated: 2026/03/20 21:44:08 by made-jes         ###   ########.fr       */
+/*   Updated: 2026/04/14 20:43:13 by made-jes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,4 +38,71 @@ t_ast	*build_ast_range(t_token *start, t_token *end)
 t_ast	*build_ast(t_token *tokens)
 {
 	return (build_ast_range(tokens, NULL));
+}
+
+int	count_args(t_token *start, t_token *end)
+{
+	int		count;
+	t_token	*tokens;
+
+	count = 0;
+	tokens = start;
+	while (tokens && tokens != end)
+	{
+		if (tokens->type == WORD || tokens->type == STR)
+			count++;
+		else if (tokens->type == REDIR_IN || tokens->type == REDIR_OUT
+			|| tokens->type == APPEND || tokens->type == HEREDOC)
+			tokens = tokens->next;
+		tokens = tokens->next;
+	}
+	return (count);
+}
+
+void	fill_args(t_ast *node, t_token *start, t_token *end)
+{
+	int		i;
+	t_token	*tokens;
+
+	i = 0;
+	tokens = start;
+	while (tokens && tokens != end)
+	{
+		if (tokens->type == WORD || tokens->type == STR)
+		{
+			if (tokens->value[0] != '\0' || tokens->was_quoted)
+				node->cmd_args[i++] = ft_strdup(tokens->value);
+		}
+		else if (tokens->type == REDIR_IN || tokens->type == REDIR_OUT
+			|| tokens->type == APPEND || tokens->type == HEREDOC)
+			tokens = tokens->next;
+		tokens = tokens->next;
+	}
+	node->cmd_args[i] = NULL;
+}
+
+void	add_redir(t_ast *node, int type, char *filename, int expand)
+{
+	t_redir	*redir;
+	t_redir	*current;
+
+	redir = malloc(sizeof(t_redir));
+	if (!redir)
+		return ;
+	redir->type = type;
+	redir->filename = ft_strdup(filename);
+	redir->expand = expand;
+	redir->delimiter = NULL;
+	redir->quoted = 0;
+	redir->heredoc_file = NULL;
+	redir->next = NULL;
+	if (!node->redirs)
+		node->redirs = redir;
+	else
+	{
+		current = node->redirs;
+		while (current->next)
+			current = current->next;
+		current->next = redir;
+	}
 }
