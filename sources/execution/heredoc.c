fe307a361a59a1e23a94d548ffbf01994fb0cdb3
made-jes <chivas.maria17@hotmail.com>
2026-04-18 22:00:04 +0100

    warning for sigquit inside heredoc

--- sources/execution/heredoc.c
+++ sources/execution/heredoc.c
@@ -6,98 +6,49 @@
 /*   By: made-jes <made-jes@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/28 12:04:28 by mlucena-          #+#    #+#             */
-/*   Updated: 2026/04/18 01:04:30 by made-jes         ###   ########.fr       */
+/*   Updated: 2026/04/18 21:59:09 by made-jes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-void	here_doc_read(int file, char *del, int should_expand, char *filename)
+static void	print_heredoc_warning(int line_count, char *delimiter)
+{
+	ft_putstr_fd("minishell: warning: here-document at line ", 2);
+	ft_putnbr_fd(line_count, 2);
+	ft_putstr_fd(" delimited by end-of-file (wanted `", 2);
+	ft_putstr_fd(delimiter, 2);
+	ft_putstr_fd("')\n", 2);
+}
+
+static int	process_heredoc_input(int file, char *del, int should_expand)
 {
 	char	*line;
+	int		line_count;
 
-	get_shell()->heredoc_tmp_file = filename;
-	signal(SIGINT, heredoc_sigint);
+	line_count = 1;
 	while (1)
 	{
 		line = readline("> ");
 		if (!line)
-			break ;
+			return (print_heredoc_warning(line_count, del), line_count);
 		if (ft_strncmp(line, del, strlen(del)) == 0
 			&& strlen(line) == strlen(del))
-		{
-			free(line);
-			break ;
-		}
+			return (free(line), line_count);
 		process_heredoc_line(file, line, should_expand);
 		free(line);
+		line_count++;
 	}
+}
+
+void	here_doc_read(int file, char *del, int should_expand, char *filename)
+{
+	get_shell()->heredoc_tmp_file = filename;
+	signal(SIGINT, heredoc_sigint);
+	process_heredoc_input(file, del, should_expand);
 	close(file);
 	free(filename);
 	get_shell()->heredoc_tmp_file = NULL;
-}
-
-char	*create_tmp_filename(void)
-{
-	static int	i;
-	char		*name;
-	char		*num;
-	char		*tmp;
-
-	num = ft_itoa(i++);
-	if (!num)
-		return (NULL);
-	tmp = ft_strjoin("/tmp/minishell_heredoc_", num);
-	free(num);
-	if (!tmp)
-		return (NULL);
-	name = tmp;
-	return (name);
-}
-
-int	handle_here_doc(t_redir *redir)
-{
-	int		pid;
-	int		file;
-	int		status;
-	char	*filename;
-
-	file = prepare_here_doc_file(&filename);
-	if (file < 0)
-		return (-1);
-	pid = spawn_here_doc_reader(redir, file, filename);
-	if (pid < 0)
-		return (-1);
-	waitpid(pid, &status, 0);
-	close(file);
-	return (finish_here_doc(redir, filename, status));
-}
-
-int	handle_here_doc_tree(t_ast *ast)
-{
-	t_redir	*aux;
-	int		status;
-
-	if (ast)
-	{
-		aux = ast->redirs;
-		while (aux)
-		{
-			if (aux->type == HEREDOC)
-			{
-				status = handle_here_doc(aux);
-				if (status != 0)
-					return (1);
-			}
-			aux = aux->next;
-		}
-		if (handle_here_doc_tree(ast->left))
-			return (1);
-		if (handle_here_doc_tree(ast->right))
-			return (1);
-		return (0);
-	}
-	return (0);
 }
 
 int	here_doc_execution(t_redir *redir)
--- sources/execution/heredoc2.c
+++ sources/execution/heredoc2.c
@@ -6,7 +6,7 @@
 /*   By: made-jes <made-jes@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/11 22:29:19 by made-jes          #+#    #+#             */
-/*   Updated: 2026/04/11 22:36:31 by made-jes         ###   ########.fr       */
+/*   Updated: 2026/04/18 21:59:03 by made-jes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,4 +25,67 @@ void	process_heredoc_line(int file, char *line, int should_expand)
 		write(file, line, ft_strlen(line));
 	write(file, "\n", 1);
 	free(expanded_line);
+}
+
+char	*create_tmp_filename(void)
+{
+	static int	i;
+	char		*name;
+	char		*num;
+	char		*tmp;
+
+	num = ft_itoa(i++);
+	if (!num)
+		return (NULL);
+	tmp = ft_strjoin("/tmp/minishell_heredoc_", num);
+	free(num);
+	if (!tmp)
+		return (NULL);
+	name = tmp;
+	return (name);
+}
+
+int	handle_here_doc(t_redir *redir)
+{
+	int		pid;
+	int		file;
+	int		status;
+	char	*filename;
+
+	file = prepare_here_doc_file(&filename);
+	if (file < 0)
+		return (-1);
+	pid = spawn_here_doc_reader(redir, file, filename);
+	if (pid < 0)
+		return (-1);
+	waitpid(pid, &status, 0);
+	close(file);
+	return (finish_here_doc(redir, filename, status));
+}
+
+int	handle_here_doc_tree(t_ast *ast)
+{
+	t_redir	*aux;
+	int		status;
+
+	if (ast)
+	{
+		aux = ast->redirs;
+		while (aux)
+		{
+			if (aux->type == HEREDOC)
+			{
+				status = handle_here_doc(aux);
+				if (status != 0)
+					return (1);
+			}
+			aux = aux->next;
+		}
+		if (handle_here_doc_tree(ast->left))
+			return (1);
+		if (handle_here_doc_tree(ast->right))
+			return (1);
+		return (0);
+	}
+	return (0);
 }
