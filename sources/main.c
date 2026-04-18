78954e51e9f93b09c295f53806df45895f3cf044
made-jes <chivas.maria17@hotmail.com>
2026-04-18 21:28:27 +0100

    protected for more args than it should

--- sources/main.c
+++ sources/main.c
@@ -6,7 +6,7 @@
 /*   By: made-jes <made-jes@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/02 18:52:17 by made-jes          #+#    #+#             */
-/*   Updated: 2026/04/15 00:19:56 by made-jes         ###   ########.fr       */
+/*   Updated: 2026/04/18 21:27:27 by made-jes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,15 @@ t_shell	*get_shell(void)
 
 int	main(int ac, char **av, char **envp)
 {
-	(void)ac;
 	(void)av;
-	init_shell(envp);
-	setup_signals();
-	run_prompt();
-	cleanup_and_exit(get_shell(), get_shell()->last_exit);
+	if (ac == 1)
+	{
+		init_shell(envp);
+		setup_signals();
+		run_prompt();
+		cleanup_and_exit(get_shell(), get_shell()->last_exit);
+		return (1);
+	}
+	ft_putstr_fd("minishell: no arguments required\n", 2);
 	return (0);
 }
